159df385333ca37cfd766e9fec17b5212b2de7e5
Cote_Poblete <maria.jpoblete@gmail.com>
2025-09-15 21:49:05 -0300

    curso santander de python

---
+++ 11.1-ejemplo_funciones.py
@@ -0,0 +1,14 @@
+def calcular_media(*numeros):
+   suma = sum(numeros)
+   cantidad = len(numeros)
+   media = suma / cantidad
+   return media
+
+print("Media", calcular_media(10, 20, 30, 40))
+
+def sumar_3(x):
+    return x + 3
+
+sumar = lambda x: x + 3
+
+print("Sumar 3:", sumar(5))
---
+++ 6.1.-ejemplo_bucle_loop.py
@@ -0,0 +1,10 @@
+print("Números del 1 al 5 multiplicados por 2 conbucle for:")
+for numero in range(1,6):
+    print(numero * 2)
+
+print("\nNúmeros del 1 al 5 multiplicados por 2 con bucle while:")
+contador = 1
+
+while contador <= 5:
+    print(contador * 2)
+    contador +=1
